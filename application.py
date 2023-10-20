b1272534d43a881f9f61325bdc1e04e1172e581b
PuneetSonii <puneetsonii369@gmail.com>
2023-10-20 11:13:07 +0530

    deployment started

--- application.py
+++ application.py
@@ -14,6 +14,13 @@ application=Flask(__name__)
 
 app=application
 
+## route for a home page
+@app.route('D:/ccdp/templates/static/trr.jpg')
+def serve_file(filename):
+    return send_from_directory('static', filename)
+
+
+
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -60,13 +67,16 @@ def predict_datapoint():
 
     pred_df=data.get_data_as_data_frame()
     print(pred_df)
+    print("Before Prediction")
 
     predict_pipeline=PredictPipeline()
-    
+    print("Mid Prediction")
+
     results=predict_pipeline.predict(pred_df)
+    print("after Prediction")
     
     return render_template('home.html',results=results[0])
 
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
